dfd2e0c6d8a1633656d80afdf2eba3986dd775f3
Allan Berry <allan.berry@gmail.com>
2017-11-17 18:00:17 -0600

    add places

---
+++ places/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+from visualist.models import Record
+
+class Place(Record):
+    # TODO: think on this
+    latitude = models.DecimalField(
+        decimal_places=7,
+        max_digits=10,
+    )
+    longitude = models.DecimalField(
+        decimal_places=7,
+        max_digits=10,
+    )
+    altitude = models.DecimalField(
+        decimal_places=7,
+        max_digits=10,
+        blank=True,
+        null=True,
+    )
+    street = models.TextField(
+        blank=True,
+        null=True,
+    )
+    locality = models.CharField(
+        max_length=250,
+        blank=True,
+        null=True,
+        default='Chicago',
+    )
+    REGIONS = (
+        ('IL','Illinois'),
+        ('IN','Indiana'),
+        ('MI','Michigan'),
+        ('WI','Wisconsin'),
+    )
+    region = models.CharField(
+        choices=REGIONS,
+        max_length=250,
+        default="IL",
+        blank=True,
+        null=True,
+    )
+    postal_code = models.CharField(
+        max_length=250,
+        blank=True,
+        null=True,
+    )
+    COUNTRIES = (
+        ('CA','Canada'),
+        ('FR','France'),
+        ('MX','Mexico'),
+        ('US','United States'),
+    )
+    countries = models.CharField(
+        choices=COUNTRIES,
+        max_length=250,
+        default="US",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        street_single_line = self.street.replace('\r\n', ', ')
+        return '{}, {} {}'.format(street_single_line, self.locality, self.region)
+
+    class Meta:
+        unique_together = (
+            ("latitude", "longitude", "altitude"),
+        )
--- visualist/settings/base.py
+++ visualist/settings/base.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'visualist',
     'names',
     'events',
+    'places',
 
 ]
 
